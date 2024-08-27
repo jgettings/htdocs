f8744fefb00f6af5aa826182b565c815eaa5d6f1
Jen Gettings <jennifer.wystup@gmail.com>
2024-08-27 17:19:17 -0400

    skills icons

--- src/pages/Basics/SkillsSummary.tsx
+++ src/pages/Basics/SkillsSummary.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
-import DescriptionBox from 'components/DescriptionBox';
+import {
+  DiAtlassian,
+  DiCss3,
+  DiGithubAlt,
+  DiGoogleCloudPlatform,
+  DiJava,
+  DiHtml5,
+  DiMysql,
+  DiMongodb,
+  DiNpm,
+  DiPostgresql,
+  DiReact,
+  DiScrum,
+} from 'react-icons/di';
+import {
+  SiTypescript,
+  SiCsharp,
+  SiMui,
+  SiDocker,
+  SiJavascript,
+} from 'react-icons/si';
+import { RiFirebaseFill, RiTailwindCssFill } from 'react-icons/ri';
+import { TbBrandAzure } from 'react-icons/tb';
 
+import DescriptionBox from 'components/DescriptionBox';
 import data from 'data/index';
 
-// const icons = [
-//   faReact,
-//   faJava,
-//   faJs,
-//   faHtml5,
-//   faCss3Alt,
-//   faGithub,
-//   faJira,
-//   faDocker,
-//   faCodeBranch,
-//   faCode,
-//   faDatabase,
-// ];
+const icons = [
+  { icon: DiReact, title: 'React' },
+  { icon: SiTypescript, title: 'TypeScript' },
+  { icon: SiJavascript, title: 'JavaScript' },
+  { icon: DiHtml5, title: 'HTML' },
+  { icon: DiCss3, title: 'CSS' },
+  { icon: DiNpm, title: 'NPM' },
+  { icon: SiMui, title: 'Material UI' },
+  { icon: RiTailwindCssFill, title: 'Tailwind' },
+
+  { icon: DiAtlassian, title: 'Atlassian' },
+  { icon: DiScrum, title: 'Agile, Scrum' },
+  { icon: DiGithubAlt, title: 'Github' },
+  { icon: DiGoogleCloudPlatform, title: 'Google Cloud Platform' },
+  { icon: RiFirebaseFill, title: 'Firebase' },
+  { icon: TbBrandAzure, title: 'Azure' },
+
+  { icon: DiJava, title: 'Java' },
+  { icon: DiMysql, title: 'MySQL' },
+  { icon: DiMongodb, title: 'MongoDB' },
+  { icon: DiPostgresql, title: 'PostgreSQL' },
+  { icon: SiCsharp, title: 'CSharp, .NET Framework, .NETCore' },
+  { icon: SiDocker, title: 'Docker' },
+];
 
 const SkillsSummary: React.FC = () => {
   const { basics } = data;
@@ -27,13 +61,15 @@ const SkillsSummary: React.FC = () => {
     <DescriptionBox>
       Specializing on frontend work in React and Typescript for the past 7
       years, prior to that was all full stack in a few different languages.
-      {/* {icons.map((icon) => (
-        <FontAwesomeIcon icon={icon} />
-      ))} */}
+      <div className="flex flex-wrap justify-center gap-5">
+        {icons.map(({ icon, title }) => (
+          <span key={title}>{icon({ size: '2em', title })}</span>
+        ))}
+      </div>
     </DescriptionBox>
   );
 };
 
-// TODO add as keywords to meta tags
+// TODO add link to skills section once the section is added
 
 export default SkillsSummary;
