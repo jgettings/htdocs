f36e865a7990e702c79ace32821228b5527ea2f0
jgettings <jgettings@users.noreply.github.com>
2024-10-02 23:20:24 +0000

    deploy: ef19b6db9e09dd35460c902efd0ec2e1c37d9a8b

---
+++ assets/API_Development-Yt34Sqvz.js
@@ -0,0 +1,4 @@
+const e={},t=`While I've been focused on the frontend at my last couple of roles, I still have had a hand in designing the API contracts needed to get various features working. In this capacity I've worked with both RESTful and GraphQL APIs and I've also helped make minor adjustments to them as needed in Python and Kotlin.
+
+In full stack roles I've worked extensively with REST APIs and APIs that return a custom set of data tailored to the frontend without following a standard like REST. Most of this has been in C# and Java. I've also worked with many webapps that were pure MVC or a mix of MVC with some javascript on the frontend for interactivity.
+`;export{e as attributes,t as markdown};
