8c494ba23e44241cb54d2a5facb61ef8f153250b
Jen Gettings <jennifer.wystup@gmail.com>
2024-10-02 20:01:55 -0400

    Refactor work timeline item into separate component

---
+++ src/pages/ResumeTimeline/WorkTimelineItem.tsx
@@ -0,0 +1,52 @@
+import { PiBuildingOfficeFill } from 'react-icons/pi';
+import { Timeline, Card } from 'flowbite-react';
+import { ResumeProfile } from 'data';
+import ResumeTimelineDates from './Dates';
+
+type WorkTimelineItemProps = {
+  work: NonNullable<ResumeProfile['work']>[0];
+};
+
+const logoWidth = 150;
+
+const WorkTimelineItem: React.FC<WorkTimelineItemProps> = ({ work }) => (
+  <Timeline.Item key={work.name}>
+    <Timeline.Point icon={PiBuildingOfficeFill} />
+    <Timeline.Content>
+      <Card>
+        <ResumeTimelineDates
+          startDate={work.startDate as string}
+          endDate={work.endDate as string}
+        />
+        <Timeline.Title className="flex flex-col justify-between gap-2 md:flex-row">
+          <div>
+            {work.position}
+            <div className="font-light italic">{work.name}</div>
+          </div>
+          <div className="flex-none">
+            <a href={work.url}>
+              {work.iconDark && (
+                <img
+                  alt={`${work.name} logo`}
+                  src={`/images/company-icons/${work.iconDark}`}
+                  width={logoWidth}
+                  className="hidden dark:inline"
+                />
+              )}
+              <img
+                alt={`${work.name} logo`}
+                src={`/images/company-icons/${work.icon}`}
+                width={logoWidth}
+                className={work.iconDark ? 'inline dark:hidden' : undefined}
+              />
+            </a>
+          </div>
+        </Timeline.Title>
+        <Timeline.Body>{work.summary}</Timeline.Body>
+        {/* <Button>Read more</Button> */}
+      </Card>
+    </Timeline.Content>
+  </Timeline.Item>
+);
+
+export default WorkTimelineItem;
--- src/pages/ResumeTimeline/index.tsx
+++ src/pages/ResumeTimeline/index.tsx
@@ -1,57 +1,14 @@
 import data from 'data/index';
-import { Timeline, Card } from 'flowbite-react';
-import { PiBuildingOfficeFill } from 'react-icons/pi';
+import { Timeline } from 'flowbite-react';
 import ScrollPageHeading from 'components/ScrollPages/Heading';
-import ResumeTimelineDates from './Dates';
-
-const logoWidth = 150;
+import WorkTimelineItem from './WorkTimelineItem';
 
 const ResumeTimeline: React.FC = () => (
   <section id="resume-timeline">
     <ScrollPageHeading>Experience</ScrollPageHeading>
     <div className="z-0 flex flex-col items-center">
       <Timeline>
-        {data.work?.map((work) => (
-          <Timeline.Item key={work.name}>
-            <Timeline.Point icon={PiBuildingOfficeFill} />
-            <Timeline.Content>
-              <Card>
-                <ResumeTimelineDates
-                  startDate={work.startDate as string}
-                  endDate={work.endDate as string}
-                />
-                <Timeline.Title className="flex flex-col justify-between gap-2 md:flex-row">
-                  <div>
-                    {work.position}
-                    <div className="font-light italic">{work.name}</div>
-                  </div>
-                  <div className="flex-none">
-                    <a href={work.url}>
-                      {work.iconDark && (
-                        <img
-                          alt={`${work.name} logo`}
-                          src={`/images/company-icons/${work.iconDark}`}
-                          width={logoWidth}
-                          className="hidden dark:inline"
-                        />
-                      )}
-                      <img
-                        alt={`${work.name} logo`}
-                        src={`/images/company-icons/${work.icon}`}
-                        width={logoWidth}
-                        className={
-                          work.iconDark ? 'inline dark:hidden' : undefined
-                        }
-                      />
-                    </a>
-                  </div>
-                </Timeline.Title>
-                <Timeline.Body>{work.summary}</Timeline.Body>
-                {/* <Button>Read more</Button> */}
-              </Card>
-            </Timeline.Content>
-          </Timeline.Item>
-        ))}
+        {data.work?.map((work) => <WorkTimelineItem work={work} />)}
       </Timeline>
     </div>
   </section>
